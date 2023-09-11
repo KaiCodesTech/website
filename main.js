3f3d3f097f410418309d26b701619de4fd672174
SpyKai0808 <kai@kaicodes.tech>
2023-09-11 16:52:19 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// Initialize the FirebaseUI Widget using Firebase.
-// var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
 var navstate = false;
 
 function nav() {
@@ -10,28 +7,3 @@ function nav() {
 function closeNav() {
     document.getElementById("sideNav").style.width = "0";
 }
-
-// ui.start('#firebaseui-auth-container', {
-//     signInOptions: [
-//         firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//         firebase.auth.GoogleAuthProvider.PROVIDER_ID
-//     ],
-//     Other config options...
-// });
-
-// const sideNav = document.getElementsByClassName("sideNav")
-//
-// const sitesFolders = fs.readdirSync("./sites")
-//
-// function createNewElement(folderName, path) {
-//     var tag = document.createElement("a")
-//     var text = document.createTextNode(`${folderName}`)
-//     tag.href = `${path}/index.html`
-//     tag.appendChild(text)
-//     var element = sideNav
-//     element.appendChild(tag)
-// }
-//
-// for (folder in sitesFolders) {
-//     createNewElement(folders, './sites')
-// }
